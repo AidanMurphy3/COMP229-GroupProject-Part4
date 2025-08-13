cc3a76877b5e7286564d16f2eb5a8e702d967fe2
BibekLama152 <kdie1277@gmail.com>
2025-08-13 14:28:07 -0400

    Update User.js
    udate the errors

--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // models/User.js
 const mongoose = require('mongoose');
 
@@ -6,7 +5,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true }, // <- must match login/register
+    passwordHash: { type: String, required: true }, // must match login/register
     role: { type: String, enum: ['patient', 'doctor', 'admin'], default: 'patient', index: true },
     specialty: { type: String }, // for doctors (optional)
     bio: { type: String }        // optional
@@ -15,21 +14,3 @@ const userSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model('User', userSchema);
-=======
-// models/User.js
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true }, // <- must match login/register
-    role: { type: String, enum: ['patient', 'doctor', 'admin'], default: 'patient', index: true },
-    specialty: { type: String }, // for doctors (optional)
-    bio: { type: String }        // optional
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model('User', userSchema);
->>>>>>> 05ca69d7910ac218c490ccf3411a4d9904e3aeb5
